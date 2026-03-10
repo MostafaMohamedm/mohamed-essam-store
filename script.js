6041578b64b75b6a4a4c851eed61ef4fa3d3d685
MostafaMohamedm <mmostafaa495@gmail.com>
2026-03-10 08:20:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ if(checkoutBtn){
         });
 
         // رقم WhatsApp
-        const phone = "+2001062835140";
+        const phone = "+2001027113090";
         const waURL = `https://api.whatsapp.com/send?phone=${phone}&text=${invoice}`;
         window.open(waURL,"_blank");
 
